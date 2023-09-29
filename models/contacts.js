90c1029487996f7ef4963cdff843d17e762ee9fa
magdalenajb <m.juszczyk_84@o2.pl>
2023-09-29 23:41:34 +0200

    authentication failed :(

--- models/contacts.js
+++ models/contacts.js
@@ -1,3 +1,5 @@
+const { Schema, model } = require("mongoose");
+
 const fs = require("fs/promises");
 const path = require("path");
 const { v4 } = require("uuid");
@@ -5,65 +7,55 @@ const { v4 } = require("uuid");
 const contactsPath = path.join(__dirname, "contacts.json");
 
 const listContacts = async () => {
-
-    const data = await fs.readFile(contactsPath);
-    const contacts = JSON.parse(data);
-    return contacts;
-
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
 };
 
 const getContactById = async (contactId) => {
-  
-    const contacts = await listContacts();
-    const result = contacts.find(({ id }) => id === contactId);
-    return result;
-
+  const contacts = await listContacts();
+  const result = contacts.find(({ id }) => id === contactId);
+  return result;
 };
 
 const removeContact = async (contactId) => {
-  
-    const contactToDelete = getContactById(contactId);
-    const contacts = await listContacts();
-    const contactIndex = contacts.findIndex(({ id }) => id === contactId);
+  const contactToDelete = getContactById(contactId);
+  const contacts = await listContacts();
+  const contactIndex = contacts.findIndex(({ id }) => id === contactId);
 
-    if (contactIndex === -1 ) {
-      return null;
-    };
+  if (contactIndex === -1) {
+    return null;
+  }
 
-    contacts.splice(contactIndex, 1);
+  contacts.splice(contactIndex, 1);
 
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return contactToDelete;
-
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contactToDelete;
 };
 
 const addContact = async (body) => {
-  
-    const contacts = await listContacts();
-    const addContact = { id: v4(), ...body };
-    contacts.push(addContact);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-    return addContact;
-
+  const contacts = await listContacts();
+  const addContact = { id: v4(), ...body };
+  contacts.push(addContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return addContact;
 };
 
 const updateContact = async (contactId, body) => {
+  const contacts = await listContacts();
+  const contactIndex = contacts.findIndex(({ id }) => id === contactId);
 
-    const contacts = await listContacts();
-    const contactIndex = contacts.findIndex(({ id }) => id === contactId);
+  if (contactIndex === -1) {
+    return null;
+  }
 
-    if (contactIndex === -1 ) {
-      return null;
-    };
+  contacts[contactIndex] = { id: contactId, ...body };
 
-    contacts[contactIndex] = { id: contactId, ...body };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  const contactUpdated = contacts[contactIndex];
 
-    const contactUpdated = contacts[contactIndex];
-
-    return contactUpdated;
-
+  return contactUpdated;
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,5 +1,52 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+require("dotenv").config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const PORT = process.env.PORT || 3000;
+const uriDb = process.env.DB_HOST;
+
+const connection = async () => {
+  try {
+    const db = await mongoose.connect(
+      "mongodb+srv://zyrafa43:IclkfzYPN6P7XOgR@cluster0.tqj7dop.mongodb.net/",
+      {
+        dbName: "db-contacts",
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("DB connected");
+
+    app.listen(3000, () => {
+      console.log(`Server runnig. Use our API on Port: ${PORT}`);
+    })
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+connection();
+
+
+
+//const PORT = 3000;
+//const uriDb = process.env.DB_HOST;
+
+//const connection = mongoose.connect(
+  //"mongodb+srv://zyrafa43:IclkfzYPN6P7XOgR@cluster0.tqj7dop.mongodb.net/",
+ // {
+   // useNewUrlParser: true,
+   // useUnifiedTopology: true,
+   // dbName: "db-contacts",
+ // }
+//);
+
+//connection
+//  .then(() => {
+//    app.listen(PORT, () => {
+//      console.log("Database connection successful");
+//      console.log(`Server running. Use our API on port: ${PORT}`);
+//    });
+//  })
+//  .catch((err) =>
+//    console.log(`Server not running. Error message: ${err.message}`)
+//  );
